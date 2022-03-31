034ea4ba3f9bd0426245c55454ffe9734d9e8716
ancnnr <ancnnr@gmail.com>
2022-03-31 14:25:28 -0300

    styling is done minus errors and form validation

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ function check_pass() {
     if (document.getElementById('pass').value ==
             document.getElementById('confirm-pass').value) {
         document.getElementById('submit').disabled = false;
+        document.getElementById('error-pw').style.display='none';
     } else {
         document.getElementById('submit').disabled = true;
+        document.getElementById('error-pw').style.display='block';
     }
+}
+
+function submitForm() {
+    
 }
