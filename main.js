a69723426c5a5f38eb777e4f784490de5ceed02f
ancnnr <ancnnr@gmail.com>
2022-03-31 22:30:33 -0300

    completed error validation messages before submit

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ function check_pass() {
     if (document.getElementById('pass').value ==
             document.getElementById('confirm-pass').value) {
         document.getElementById('submit').disabled = false;
-        document.getElementById('error-pw').style.display='none';
+        document.getElementById('error-pw-match').textContent=""
     } else {
         document.getElementById('submit').disabled = true;
-        document.getElementById('error-pw').style.display='block';
+        document.getElementById('error-pw-match').textContent="Passwords do not match!"
     }
-}
 
-function submitForm() {
-    
+    if(document.getElementById('pass').value.length>7)
+    {
+        document.getElementById('submit').disabled = false;
+        document.getElementById('error-pw-len').textContent=""
+    }
+    else {
+        document.getElementById('submit').disabled = true;
+        document.getElementById('error-pw-len').textContent="Too Short! Passwords must be at least 8 characters long."     
+    }
 }
